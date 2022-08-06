5996f66a123ba6dc0155ea154eae352fd5bc64eb
anandshekhr <66034323+anandshekhr@users.noreply.github.com>
2022-08-06 18:52:30 +0530

    Update allowed host

--- ASSIGNMENT4/ASSIGNMENT4/settings.py
+++ ASSIGNMENT4/ASSIGNMENT4/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '0!i02oj+08$brhdkw-v=gr^*)an++d+1nc4uq)&&iw#v2nn@3r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','freelance-vams.herokuapp.com']
 
 
 # Application definition
