2479de9d3e487e0e58ae4037e489c5d403f05ef9
shepherdforsuh <shepherdforsuh@gmail.com>
2022-08-07 19:10:54 +0100

    env

--- ASSIGNMENT4/settings.py
+++ ASSIGNMENT4/settings.py
@@ -16,6 +16,7 @@ import environ
 
 import django_heroku
 
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
