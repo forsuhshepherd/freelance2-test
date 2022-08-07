ba254b0c1abd847396f34ffa2bebc25b7704c138
shepherdforsuh <shepherdforsuh@gmail.com>
2022-08-07 19:46:33 +0100

    env

--- ASSIGNMENT4/settings.py
+++ ASSIGNMENT4/settings.py
@@ -20,7 +20,7 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env()
-env.read_env(str(BASE_DIR / ".env"))
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
