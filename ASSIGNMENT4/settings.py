947ef9610685bbf1df3fa70c6e7ba538d21480bd
shepherdforsuh <shepherdforsuh@gmail.com>
2022-08-07 20:43:24 +0100

    pycache

--- ASSIGNMENT4/settings.py
+++ ASSIGNMENT4/settings.py
@@ -27,7 +27,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0!i02oj+08$brhdkw-v=gr^*)an++d+1nc4uq)&&iw#v2nn@3r'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG', default=True)
