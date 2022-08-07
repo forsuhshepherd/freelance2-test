4ed7d9ddab30b971c8c62fc1c07fcace47e5c6d3
shepherdforsuh <shepherdforsuh@gmail.com>
2022-08-07 18:44:49 +0100

    stocks model

--- ws_stocks/models.py
+++ ws_stocks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from ws_sectors import Sector
 
-# Create your models here.
+
+class Stock(models.Model):
+  name = models.CharField(max_lenght=100)
+  price = models.DecimalField(max_digits=5, decimal_places=2)
+  sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+  unallocated = models.Integer()
+  total_volume = models.Integer()
